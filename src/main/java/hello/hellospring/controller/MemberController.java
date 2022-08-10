eca4642443ce32eeb887fa17303eddee45adb068
serotomin <serotomin@naver.com>
2022-08-10 20:50:18 +0900

    H

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -10,7 +10,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @Autowired //생성자에 @Autowired가 되어 있으면멤버 서비스를 스프링이 스프링컨테이너에서 가져다가 연결해줌
-    public MemberController(MemberService memberService) { //Constructor(생성자) CTRL + N 로 생성
+    public MemberController(MemberService memberService) { //Constructor(생성자 주입), CTRL + N 로 생성
         this.memberService = memberService;
     }
 }
