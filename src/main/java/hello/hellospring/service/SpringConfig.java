1afccc81a4036abdba319d252a780bd1b250f45b
serotomin <serotomin@naver.com>
2022-08-25 00:21:06 +0900

    H

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -13,7 +13,7 @@ import javax.sql.DataSource;
 public class SpringConfig {
 
 
-    /* JPA 수업에서 아래 코드 주석 처리함
+    /* JPA 수업에서 아래 코드 주석 처리
 
     private DataSource dataSource; //스프링부트가 DB에 연결할 수 있는 dataSource를 생성함
     @Autowired
@@ -21,22 +21,38 @@ public class SpringConfig {
         this.dataSource = dataSource;
     }
     */
+
+    /* 스프링 데이터 JPA 수업에서 아래 코드 주석 처리
     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+    */
+
+    private final MemberRepository memberRepository;
+
+    //@Autowired //생성자가 하나인 경우 생략가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
+
+    /* 스프링 데이터 JPA 수업에서 아래 코드 주석 처리
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }}
+        //return new JpaMemberRepository(em);
+
+    }
+    */
+}
+
